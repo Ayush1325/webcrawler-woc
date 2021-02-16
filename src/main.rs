d3f57440967a8700b0b7847dabeb519d7ffc1218
Ayush Singh <ayushsingh1325@gmail.com>
2021-02-16 18:19:44 +0530

    feat: Can Crawl domain with rudimentary threading.
    body: Uses too much memory. Need to reduce all the cloning.
          Also set a limit on the number of tasks. Not as big of a problem as
          memory though.
          Works fine for small sites.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::time::Instant;
 mod retriever;
 
 #[tokio::main]
 async fn main() {
+    let now = Instant::now();
+    println!("Started");
     retriever::temp().await;
+    println!("Time: {}", now.elapsed().as_secs());
 }
--- src/retriever.rs
+++ src/retriever.rs
@@ -1,36 +1,95 @@
-use reqwest::StatusCode;
+use futures::future;
 use reqwest::Url;
 use select::document::Document;
 use select::predicate::Name;
 use std::collections::HashSet;
 
-async fn get_page(url: &Url) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
+async fn get_page(url: &str) -> Result<reqwest::Response, reqwest::Error> {
     //! Function to make get request to a single url.
     //! Impure Function
-    let resp = reqwest::get(url.clone()).await?;
-    let status = resp.status();
-    if status != StatusCode::OK {
-        let err = Box::from(format!("GET Error Code: {}", status.as_u16()));
-        return Err(err);
+    let resp = reqwest::get(url).await?;
+    resp.error_for_status()
+    // let status = resp.status();
+    // if status != StatusCode::OK {
+    //     let err = Box::from(format!("GET Error Code: {}", status.as_u16()));
+    //     return Err(err);
+    // }
+    // Ok(resp)
+}
+
+async fn crawl_page(url: String) -> Result<HashSet<String>, reqwest::Error> {
+    let page = get_page(&url).await?;
+    let html = page.text().await?;
+    let links =
+        tokio::task::spawn_blocking(move || get_links_from_html(html.as_str(), url.as_str())).await;
+    match links {
+        Ok(links) => Ok(links),
+        Err(_) => Ok(HashSet::new()),
     }
-    Ok(resp)
 }
 
-fn crawl_url(url: &Url) {
-    // TODO
+async fn crawl_host(origin_url: &str) -> Result<HashSet<String>, Box<dyn std::error::Error>> {
+    let mut visited = HashSet::new();
+    let mut found_links = HashSet::new();
+    let mut to_crawl = HashSet::new();
+
+    to_crawl.insert(origin_url.to_string());
+
+    let mut count = 1;
+
+    while !to_crawl.is_empty() {
+        println!("Pass: {}, Tasks: {}", count, to_crawl.len());
+        count += 1;
+
+        let handles = to_crawl
+            .iter()
+            .map(|x| tokio::spawn(crawl_page(x.to_string())));
+        let response = future::join_all(handles).await;
+        let new_links: HashSet<String> = response.iter().fold(HashSet::new(), |mut acc, x| {
+            if let Ok(Ok(links)) = x {
+                acc.extend(links.clone());
+            }
+            acc
+        });
+        found_links.extend(new_links.clone());
+        visited.extend(to_crawl.clone());
+        to_crawl = new_links
+            .difference(&visited)
+            .filter(|x| comare_host(origin_url, x))
+            .map(|x| x.to_string())
+            .collect();
+    }
+    Ok(found_links)
 }
 
-fn get_links_from_html(html: &str, url: &Url) -> HashSet<Url> {
+fn comare_host(original_url: &str, url: &str) -> bool {
+    let original_url = Url::parse(original_url);
+    let url = Url::parse(url);
+    if let (Ok(url1), Ok(url2)) = (original_url, url) {
+        if let (Some(url1_host), Some(url2_host)) = (url1.host_str(), url2.host_str()) {
+            if url1_host == url2_host {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn get_links_from_html(html: &str, url: &str) -> HashSet<String> {
     //! Function to extract all links from a given html string.
     //! Pure function
-    Document::from(html)
-        .find(Name("a"))
-        .filter_map(|n| n.attr("href"))
-        .filter_map(|x| normalize_url(x, url))
-        .collect::<HashSet<Url>>()
+    let url_parsed = Url::parse(url);
+    match url_parsed {
+        Ok(url) => Document::from(html)
+            .find(Name("a"))
+            .filter_map(|n| n.attr("href"))
+            .filter_map(|x| normalize_url(x, &url))
+            .collect::<HashSet<String>>(),
+        Err(_) => HashSet::new(),
+    }
 }
 
-fn normalize_url(url: &str, base_url: &Url) -> Option<Url> {
+fn normalize_url(url: &str, base_url: &Url) -> Option<String> {
     //! Helper function to parse url in a page.
     //! Converts relative urls to full urls.
     //! Also removes javascript urls and other false urls.
@@ -43,11 +102,11 @@ fn normalize_url(url: &str, base_url: &Url) -> Option<Url> {
 
     let new_url = Url::parse(url);
     match new_url {
-        Ok(new_url) => Some(new_url),
+        Ok(new_url) => Some(new_url.to_string()),
         Err(_) => {
             let new_url = base_url.join(url);
             match new_url {
-                Ok(x) => Some(x),
+                Ok(x) => Some(x.to_string()),
                 Err(_) => None,
             }
         }
@@ -55,19 +114,22 @@ fn normalize_url(url: &str, base_url: &Url) -> Option<Url> {
 }
 
 pub async fn temp() -> () {
-    let url = Url::parse("https://www.wikipedia.org/").unwrap();
-    let page = get_page(&url).await.unwrap();
-    let html = page.text().await.unwrap();
-    let links = get_links_from_html(&html, &url);
-    links.iter().for_each(|x| println!("{}", x.as_str()));
+    // let url = "https://www.wikipedia.org/";
+    // let page = get_page(&url).await.unwrap();
+    // let html = page.text().await.unwrap();
+    // let links = get_links_from_html(&html, &url);
+    // links.iter().for_each(|x| println!("{}", x.as_str()));
+    let origin_url = "https://crawler-test.com/";
+    let links = crawl_host(origin_url).await.unwrap();
+    links.iter().for_each(|x| println!("{}", x));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn gen_hasset(arr: Vec<&str>) -> HashSet<Url> {
-        arr.iter().map(|x| Url::parse(x).unwrap()).collect()
+    fn gen_hasset(arr: Vec<&str>) -> HashSet<String> {
+        arr.iter().map(|x| x.to_string()).collect()
     }
 
     #[test]
@@ -76,7 +138,7 @@ mod tests {
                     <a href='#1'></a>
                     <a href='123.html'></a>
                     <a href='https://test2.com'></a>";
-        let url = Url::parse("https://test.com/home/").unwrap();
+        let url = "https://test.com/home/";
         let links = get_links_from_html(html, &url);
         let ans = gen_hasset(vec![
             "https://test.com/123.html",
